4074f7c9bcc8a95f5e0d2c7d09691fe465ab03c0
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-04-12 01:24:43 -0400

    deploying

--- expenses_tracker/settings.py
+++ expenses_tracker/settings.py
@@ -84,17 +84,17 @@ WSGI_APPLICATION = 'expenses_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #     'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'django_api',
-    #     'USER': 'nour',
-    #     'PASSWORD': '',
-    #     'HOST': 'localhost'
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_api',
+        'USER': 'nour',
+        'PASSWORD': '',
+        'HOST': 'localhost'
+    }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600) # add this
