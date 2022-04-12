fde7be8c5569f647aa9257faf8626a33a61ea137
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-04-12 01:27:26 -0400

    fixed model

--- expenses_api/views.py
+++ expenses_api/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
 from .serializers import ExpensesSerializer
-from .models import Contact
+from .models import Expenses
 
 class ExpensesList(generics.ListCreateAPIView):
-    queryset = Contact.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
+    queryset = Expenses.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
     serializer_class = ExpensesSerializer # tell django what serializer to use
 
 class ExpensesDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Contact.objects.all().order_by('id')
+    queryset = Expenses.objects.all().order_by('id')
     serializer_class = ExpensesSerializer
