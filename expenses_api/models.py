ccf64f0fd84a97e20072ca211c36f79e2fd20341
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-04-14 23:55:59 -0400

    fixed model

--- expenses_api/models.py
+++ expenses_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Expenses(models.Model):
-    date = models.DateTimeField()
+    date = models.DateField()
     category = models.CharField(max_length=40)
     details = models.CharField(max_length=200) 
     amount = models.IntegerField()
