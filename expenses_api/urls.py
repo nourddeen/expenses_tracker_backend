c67ebe9341d4b23e1c6356f53fd5cc987e0bfe92
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-04-15 00:15:25 -0400

    fixed

--- expenses_api/urls.py
+++ expenses_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ExpensesList.as_view(), name='Expenes_list'), # api/contacts will be routed to the ContactList view for handling
+    path('/', views.ExpensesList.as_view(), name='Expenes_list'), # api/contacts will be routed to the ContactList view for handling
     path('/<int:pk>', views.ExpensesDetail.as_view(), name='Expenses_detail'), # api/contacts will be routed to the ContactDetail view for handling
 ]
