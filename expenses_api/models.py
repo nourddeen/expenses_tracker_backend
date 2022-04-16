577f1a8f64e59326500d95b2884172bf109b443a
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-04-16 02:02:14 -0400

    fixed foreign key

--- expenses_api/models.py
+++ expenses_api/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# 
+class Category(models.Model):
+    # expense = models.ForeignKey(Expenses, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+
 class Expenses(models.Model):
     date = models.DateField()
-    # category = models.CharField(max_length=40)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     details = models.CharField(max_length=200) 
     amount = models.IntegerField()
 
-class Category(models.Model):
-    expense = models.ForeignKey(Expenses, on_delete=models.CASCADE)
-    name = models.CharField(max_length=40)
--- expenses_api/serializers.py
+++ expenses_api/serializers.py
@@ -9,9 +9,9 @@ from .models import Expenses, Category
 class ExpensesSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Expenses # tell django which model to use
-        fields = ('id', 'date','details','amount',) # tell django which fields to include
+        fields = ('id', 'date','details','amount','category') # tell django which fields to include
 
 class CategorySerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Category # tell django which model to use
-        fields = ('id', 'name','expense') # tell django which fields to include
+        fields = ('id', 'name',) # tell django which fields to include
