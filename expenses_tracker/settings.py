8af4de4d0bb631de09c52a7f6603409db260e4d8
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-05-01 23:19:04 -0400

    test

--- expenses_tracker/settings.py
+++ expenses_tracker/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         'NAME': 'django_api',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': 'vast-meadow-87338.herokuapp.com'
+        'HOST': 'localhost'
     }
 }
 
